3ba0721d8ebc5c65399e5399a8d2b909ffecc284
Marcin M <m-maciejewski@hotmail.com>
2022-07-11 21:08:24 +0200

    core

---
+++ Geolocation.Core/Geolocation.Core/Domain/GeolocationData.cs
@@ -0,0 +1,13 @@
+﻿namespace Geolocation.Core.Domain
+{
+    public class GeolocationData
+    {
+        public string Ip { get; set; }
+        public int Port { get; set; }
+        public string Type { get; set; }
+        public string City { get; set; }
+        public string Zip { get; set; }
+        public float Latitude { get; set; }
+        public float Longitude { get; set; }
+    }
+}
---
+++ Geolocation.Core/Geolocation.Core/Repository/IGeoLocationRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Geolocation.Core.Repository
+{
+    public interface IGeoLocationRepository
+    {
+        Task AddAsync(Domain.GeolocationData location);
+        Task<Domain.GeolocationData> GetAllAsync();
+    }
+}
